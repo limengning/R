80d8f6639ab21d94caf5859376950ae78573e1be
limengning <limengning@shinetechchina.com>
2019-01-06 16:00:33 +0800

    R

---
+++ 6-1.R
@@ -0,0 +1,10 @@
+library(vcd)
+counts<-table(Arthritis$Improved)
+barplot(counts,
+        main="Simple Bar Plot",
+        xlab="Improvement", ylab="Frequency")
+
+barplot(counts,
+        main="Horizontal Bar Plot",
+        xlab="Frequency", ylab="Improvement",
+        horiz=TRUE)
